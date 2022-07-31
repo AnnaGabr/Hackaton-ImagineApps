69162f71268cd8fd7856d56bba561dff700f260a
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-07-31 09:37:33 -0500

    estilos vista restaurante

--- src/components/Restaurante.jsx
+++ src/components/Restaurante.jsx
@@ -7,28 +7,24 @@ export const Restaurante = ({ alimentos }) => {
 
 
   return (
-    <div >
-      <form className='alimentos-dia'>
-        ALIMENTOS DEL DÍA
-        ¡Recuerda!, aquí debes agregar los alimentos que preparaste hoy y no alcanzaste a vender.
-        ¡Entre todos nos ayudamos!
-        <button className='boton-agregar'>
-          +
-        </button>
-      </form>
-      <AlimentosDia />
-      
-      <form className='alimentos-perecederos'>
-        ALIMENTOS PERECEDEROS
-        ¡Recuerda!, aquí puedes agregar cualquier alimento que tenga una fecha de vencimiento pronta pero que no haya sido abierto, 
-        así podrás ofrecerlo a un buen precio.
-        ¡Entre todos nos ayudamos!
-        <button className='boton-agregar'>
-          +
-        </button>
-      </form>
-
+    <div className='principalForm'>
+      <div>
+        <form className='alimentos-dia'>
+          <h3>ALIMENTOS DEL DÍA</h3><br></br>
+          <p>¡Recuerda!, aquí debes agregar los alimentos que preparaste hoy y no alcanzaste a vender.
+          ¡Entre todos nos ayudamos!</p> 
+        </form>
+        <AlimentosDia />
+      </div>
+      <div>
+        <form className='alimentos-perecederos'>
+          <h3>ALIMENTOS PERECEDEROS</h3> <br></br>
+          <p>¡Recuerda!, aquí puedes agregar cualquier alimento que tenga una fecha de vencimiento pronta pero que no haya sido abierto, 
+          así podrás ofrecerlo a un buen precio.
+          ¡Entre todos nos ayudamos!</p> 
+        </form>
       <AlimentosPerecederos />
+      </div>
 
     </div>
   )
